d683e011dd93e7cc25dcbfca4a800796b001d2d3
Muhammad <muhammad87711@gmail.com>
2024-04-07 17:15:51 -0400

    Implement key handlers for paddle movement.

--- script.js
+++ script.js
@@ -30,3 +30,19 @@ let leftPressed = false;
 
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
+
+function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = true;
+    }
+  }
+  
+  function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = false;
+    }
+  }
